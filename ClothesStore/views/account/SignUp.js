4b897749eb12ee00d33acdff23b66f1523bebd1e
NQD23081 <nqd230899@gmail.com>
2024-11-18 10:34:32 +0700

    update remove password from document firebase

--- ClothesStore/views/account/SignUp.js
+++ ClothesStore/views/account/SignUp.js
@@ -62,7 +62,6 @@ export default function SignUp() {
       await setDoc(userDoc, {
         username: username,
         email: email,
-        password: password,
         phone: phone,
       });
 
