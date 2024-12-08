5a3fc2eaf63a0c25c55a3daa0168ec89cf8e51e3
thai03127 <thai.ng03127@sinhvien.hoasen.edu.vn>
2024-12-08 13:21:06 +0700

    Update view Home

--- ClothesStore/views/store/Home.js
+++ ClothesStore/views/store/Home.js
@@ -9,16 +9,19 @@ import {
   TextInput,
   TouchableOpacity,
   Dimensions,
+  Modal,
+  TouchableWithoutFeedback,
+  ScrollView,
+  onLayoutChange
 } from "react-native";
 import { useNavigation } from "@react-navigation/native";
-import Icon from "react-native-vector-icons/Ionicons"; // Sử dụng icon từ Ionicons
+import FontAwesome from "react-native-vector-icons/FontAwesome6"; // Sử dụng FontAwesome từ FontAwesome6
 import { FIREBASE_DB } from "../../firebaseConfig";
 import { collection, getDocs, query, where } from "firebase/firestore";
 import {
   BottomSheetModal,
   BottomSheetModalProvider,
 } from "@gorhom/bottom-sheet";
-
 const { width } = Dimensions.get("window");
 const CARD_WIDTH = width / 2 - 20;
 
@@ -30,12 +33,15 @@ export default function Home() {
   const [sortOrder, setSortOrder] = useState("asc"); // Thứ tự sắp xếp: 'asc' hoặc 'desc'
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [layoutMode, setLayoutMode] = useState(1); // Mặc định 1 sản phẩm trên 1 hàng
 
   const bottomSheetModalRef = useRef(null);
   const snapPoints = useMemo(() => ["25%", "50%"], []);
 
   // Lấy dữ liệu từ Firestore
   useEffect(() => {
+
     const fetchProducts = async () => {
       try {
         const querySnapshot = await getDocs(collection(FIREBASE_DB, "Product"));
@@ -79,6 +85,9 @@ export default function Home() {
   };
 
   // Xử lý tìm kiếm
+  const handleFilteredProducts = (products) => {
+    setFilteredProducts(products);
+  };
   const handleSearch = () => {
     const keyword = searchKeyword.trim().toLowerCase();
     if (keyword === "") {
@@ -108,6 +117,7 @@ export default function Home() {
     });
     setFilteredProducts(sortedProducts);
     setSortOrder(sortOrder === "asc" ? "desc" : "asc"); // Đổi thứ tự
+    setModalVisible(false); // Đóng Modal sau khi chọn
   };
 
   // Lọc sản phẩm theo loại
@@ -154,7 +164,127 @@ export default function Home() {
   return (
     <BottomSheetModalProvider>
       <SafeAreaView style={styles.container}>
-        <Text style={styles.title}>Danh sách sản phẩm</Text>
+        {/* Header */}
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Search</Text>
+          {/* TextInput không khung viền */}
+          <View style={styles.searchContainer}>
+            {searchKeyword.length > 0 && (
+              <TouchableOpacity onPress={handleSearch} style={styles.searchButton}>
+                <FontAwesome name="magnifying-glass" size={17} color="#708090" />
+              </TouchableOpacity>
+            )}
+            <TextInput
+              style={styles.searchInput}
+              value={searchKeyword}
+              onChangeText={setSearchKeyword}
+            />
+            {searchKeyword.length > 0 && (
+              <TouchableOpacity
+                onPress={handleClearSearch}
+                style={styles.clearIcon}
+              >
+                <FontAwesome name="xmark" size={17} color="#708090" />
+              </TouchableOpacity>
+            )}
+
+          </View>
+          {/* Header TouchableOpacity */}
+          <View style={styles.headerIcons}>
+            <TouchableOpacity>
+              <FontAwesome name="heart" size={20} color="#000" style={styles.icon} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => setModalVisible(true)} >
+              <FontAwesome name="filter" size={20} color="#000" style={styles.icon} />
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <FontAwesome name="bag-shopping" size={20} color="#000" style={styles.icon} />
+            </TouchableOpacity>
+          </View>
+        </View>
+
+        {/* Filter Modal */}
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => setModalVisible(false)} // Đóng Modal khi nhấn ngoài
+        >
+          <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+            <View style={styles.modalOverlay}>
+              <TouchableWithoutFeedback>
+                <View style={styles.modalContainer}>
+                  <Text style={styles.modalTitle}>Filter</Text>
+
+                  {/* Sort Price */}
+                  <View style={styles.rowContainer}>
+                    <Text style={styles.titleLabel}>Price:</Text>
+                    <View style={styles.buttonContainer}>
+                      <TouchableOpacity
+                        style={[
+                          styles.sortButton,
+                          sortOrder === "asc" && styles.buttonSelectedUp,
+                        ]}
+                        onPress={() => handleSort("asc")}
+                      >
+                        <FontAwesome name="sort-up" size={20} color="#000" />
+                      </TouchableOpacity>
+
+                      <TouchableOpacity
+                        style={[
+                          styles.sortButton,
+                          sortOrder === "desc" && styles.buttonSelectedDown,
+                        ]}
+                        onPress={() => handleSort("desc")}
+                      >
+                        <FontAwesome name="sort-down" size={20} color="#000" />
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+
+                  {/* Change Layout */}
+                  <View style={styles.rowContainer}>
+                    <Text style={styles.titleLabel}>Layout:</Text>
+                    <View style={styles.layoutBtnGroup}>
+                      <TouchableOpacity
+                        style={[
+                          styles.layoutButton,
+                          layoutMode === 1 && styles.layoutButtonSelected,
+                        ]}
+                        onPress={() => setLayoutMode(1) & setModalVisible(false)}
+                      >
+                        <Text style={styles.layoutText}>1</Text>
+                      </TouchableOpacity>
+
+                      <TouchableOpacity
+                        style={[
+                          styles.layoutButton,
+                          layoutMode === 2 && styles.layoutButtonSelected,
+                        ]}
+                        onPress={() => setLayoutMode(2) & setModalVisible(false)}
+                      >
+                        <Text style={styles.layoutText}>2</Text>
+                      </TouchableOpacity>
+
+                      <TouchableOpacity
+                        style={[
+                          styles.layoutButton,
+                          layoutMode === 4 && styles.layoutButtonSelected,
+                        ]}
+                        onPress={() => setLayoutMode(4) & setModalVisible(false)}
+                      >
+                        <Text style={styles.layoutText}>4</Text>
+                      </TouchableOpacity>
+                    </View>
+                  </View>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          </TouchableWithoutFeedback>
+        </Modal>
+
+        {/* Content */}
+        {/* <Text style={styles.title}>Danh sách sản phẩm</Text>
         <View style={styles.searchContainer}>
           <TextInput
             style={styles.searchInput}
@@ -178,25 +308,58 @@ export default function Home() {
               {sortOrder === "asc" ? "Giá ↑" : "Giá ↓"}
             </Text>
           </TouchableOpacity>
-        </View>
-        {/* Nút lọc loại sản phẩm */}
+        </View> */}
+
+        {/* Category */}
         <TouchableOpacity
-          style={styles.filterButton}
+          style={styles.categoryButton}
           onPress={openCategorySheet}
         >
-          <Text style={styles.filterButtonText}>
+          <Text style={styles.categoryButtonText}>
             {selectedCategory
-              ? `Loại: ${getCategoryName(selectedCategory)}`
-              : "Chọn loại"}
+              ? `Category: ${getCategoryName(selectedCategory)}`
+              : "Category"}
           </Text>
-          <Icon name="filter" size={20} color="#fff" />
+          <FontAwesome name="layer-group" size={20} color="#fff" />
         </TouchableOpacity>
-        <FlatList
+
+        {/* Render Item */}
+        <ScrollView>
+          <View style={styles.productList}>
+            {filteredProducts.map((item, index) => (
+              <View
+                key={index}
+                style={[
+                  styles.productContainer,
+                  layoutMode === 1 && { width: "100%" }, // 1 sản phẩm trên 1 hàng
+                  layoutMode === 2 && { width: "48%", marginHorizontal: "1%" }, // 2 sản phẩm trên 1 hàng
+                  layoutMode === 4 && { width: "23%", marginHorizontal: "1%" }, // 4 sản phẩm trên 1 hàng
+                ]}
+              >
+                <TouchableOpacity onPress={() => navigation.navigate("Detail", { productId: item.id })}>
+                  <Image source={{ uri: item.images[0] }} style={styles.productImage} />
+                  <Text style={styles.productName}>{item.productName}</Text>
+                  <Text style={styles.productPrice}>
+                    {parseInt(item.price).toLocaleString("vi-VN")} {item.priceUnit}
+                  </Text>
+                </TouchableOpacity>
+
+                {/* <Text style={styles.productDescription}>{item.description}</Text> */}
+              </View>
+            ))}
+          </View>
+        </ScrollView >
+
+
+        {/* <FlatList
           data={filteredProducts}
           renderItem={renderItem}
+          numColumns={layoutMode === 1 ? 1 : layoutMode === 2 ? 2 : 4}
+          key={layoutMode} // Thêm key để FlatList re-render khi layoutMode thay đổi
           keyExtractor={(item) => item.id}
           contentContainerStyle={styles.list}
-        />
+        /> */}
+
         {/* Bottom Sheet */}
         <BottomSheetModal
           ref={bottomSheetModalRef}
@@ -204,20 +367,7 @@ export default function Home() {
           snapPoints={snapPoints}
         >
           <View style={styles.bottomSheetContainer}>
-            <Text style={styles.sheetTitle}>Chọn loại sản phẩm</Text>
-
-            {/* Nút bỏ chọn loại */}
-            <TouchableOpacity
-              style={styles.clearCategoryButton}
-              onPress={() => {
-                setFilteredProducts(products); // Hiển thị lại tất cả sản phẩm
-                setSelectedCategory(null); // Xóa loại sản phẩm đã chọn
-                bottomSheetModalRef.current?.dismiss(); // Đóng Bottom Sheet
-              }}
-            >
-              <Text style={styles.clearCategoryButtonText}>Bỏ chọn loại</Text>
-            </TouchableOpacity>
-
+            <Text style={styles.sheetTitle}>Choose Category</Text>
             {categories.map((category) => (
               <TouchableOpacity
                 key={category.id}
@@ -227,6 +377,17 @@ export default function Home() {
                 <Text style={styles.categoryText}>{category.Name}</Text>
               </TouchableOpacity>
             ))}
+            {/* clearCategoryButton */}
+            <TouchableOpacity
+              style={styles.clearCategoryButton}
+              onPress={() => {
+                setFilteredProducts(products); // Hiển thị lại tất cả sản phẩm
+                setSelectedCategory(null); // Xóa loại sản phẩm đã chọn
+                bottomSheetModalRef.current?.dismiss(); // Đóng Bottom Sheet
+              }}
+            >
+              <Text style={styles.clearCategoryButtonText}>Cancel</Text>
+            </TouchableOpacity>
           </View>
         </BottomSheetModal>
       </SafeAreaView>
@@ -240,90 +401,230 @@ const styles = StyleSheet.create({
     backgroundColor: "#f9f9f9",
     padding: 10,
   },
-  title: {
-    fontSize: 24,
-    fontWeight: "bold",
-    textAlign: "center",
-    marginVertical: 10,
+  // Header Style
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ddd',
+  },
+  headerTitle: {
+    fontSize: 18,
+    flex: 0.5,
+    textAlign: 'left',
+    color: '#333',
   },
   searchContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 10,
-    borderWidth: 1,
-    borderColor: "#ddd",
-    borderRadius: 8,
-    paddingHorizontal: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+    flex: 1.2,
+    marginLeft: 5,
+    marginRight: 5,
+    borderBottomWidth: 1,
+    borderBottomColor: '#00000',
   },
   searchInput: {
     flex: 1,
-    height: 40,
-    fontSize: 16,
-  },
-  clearIcon: {
-    marginLeft: 5,
+    fontSize: 17,
+    color: '#333',
+    padding: 0,
   },
   searchButton: {
-    backgroundColor: "#007bff",
-    borderRadius: 8,
-    padding: 10,
-    marginLeft: 5,
+    // marginLeft: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  headerIcons: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  icon: {
+    marginHorizontal: 8,
+  },
+
+  // title: {
+  //   fontSize: 24,
+  //   fontWeight: "bold",
+  //   textAlign: "center",
+  //   marginVertical: 10,
+  // },
+  // searchContainer: {
+  //   flexDirection: "row",
+  //   alignItems: "center",
+  //   marginBottom: 10,
+  //   borderWidth: 1,
+  //   borderColor: "#ddd",
+  //   borderRadius: 8,
+  //   paddingHorizontal: 10,
+  // },
+  // searchInput: {
+  //   flex: 1,
+  //   height: 40,
+  //   fontSize: 16,
+  // },
+  // clearIcon: {
+  //   marginLeft: 5,
+  // },
+  // searchButton: {
+  //   backgroundColor: "#007bff",
+  //   borderRadius: 8,
+  //   padding: 10,
+  //   marginLeft: 5,
+  // },
+  // sortButton: {
+  //   backgroundColor: "#28a745",
+  //   borderRadius: 8,
+  //   padding: 10,
+  //   marginLeft: 5,
+  // },
+  // sortButtonText: {
+  //   color: "#fff",
+  //   fontWeight: "bold",
+  // },
+
+  //Modal
+  modalOverlay: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContainer: {
+    width: "80%",
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    alignItems: "center",
+  },
+  modalTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 20,
+    borderBottomColor: "#000000",
+    borderBottomWidth: 1,
+  },
+  rowContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    width: "100%",
+    marginBottom: 20,
+  },
+  titleLabel: {
+    fontSize: 16,
+    fontWeight: "bold",
+    marginRight: 10,
+  },
+  buttonContainer: {
+    flexDirection: "row",
   },
   sortButton: {
-    backgroundColor: "#28a745",
-    borderRadius: 8,
-    padding: 10,
-    marginLeft: 5,
-  },
-  sortButtonText: {
-    color: "#fff",
-    fontWeight: "bold",
-  },
-  filterButton: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "#007bff",
-    padding: 10,
-    borderRadius: 8,
-    marginBottom: 10,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    marginHorizontal: 5,
+    backgroundColor: "#f0f0f0",
+    borderRadius: 5,
+    shadowColor: '#000', // Tạo hiệu ứng đổ bóng
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
   },
-  filterButtonText: {
+  buttonSelectedUp: {
+    backgroundColor: "#4CAF50",
+  },
+  buttonSelectedDown: {
+    backgroundColor: "#dc143c",
+  },
+  layoutBtnGroup: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 10,
+  },
+  layoutButton: {
+    padding: 10,
+    marginHorizontal: 6,
+    borderRadius: 8,
+    backgroundColor: "#e0e0e0",
+    shadowColor: '#000', // Tạo hiệu ứng đổ bóng
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+  },
+  layoutButtonSelected: {
+    backgroundColor: "#4CAF50",
+  },
+  layoutText: {
+    color: "#000",
+    fontWeight: "bold",
+    fontSize: 15
+  },
+
+  // Category
+  categoryButton: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#708090",
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 10,
+    shadowColor: '#000', // Tạo hiệu ứng đổ bóng
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+  },
+  categoryButtonText: {
     color: "#fff",
     fontWeight: "bold",
     marginRight: 5,
   },
-  list: {
-    paddingBottom: 20,
+
+  //Render Item
+  productList: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    marginBottom: 20,
   },
-  productCard: {
-    width: CARD_WIDTH,
-    backgroundColor: "#fff",
-    borderRadius: 8,
-    padding: 10,
-    shadowColor: "#000",
-    shadowOpacity: 0.1,
-    shadowRadius: 5,
-    elevation: 3,
-    alignItems: "center",
+  productContainer: {
+    marginVertical: 15,
+    paddingBottom: 15,
+    alignItems: 'center', // Căn giữa toàn bộ container sản phẩm
   },
   productImage: {
-    width: "100%",
-    height: 120,
-    resizeMode: "cover",
-    borderRadius: 8,
+    width: '100%',
+    height: undefined,
+    aspectRatio: 2 / 3,  // Tạo tỷ lệ hình ảnh 2:3
+    resizeMode: 'cover', // Đảm bảo hình ảnh bao phủ toàn bộ không gian
   },
   productName: {
-    fontSize: 16,
-    fontWeight: "bold",
     marginTop: 10,
-    textAlign: "center",
+    fontSize: 13,
+    fontWeight: '600',
+    textAlign: 'center',  // Căn giữa tên sản phẩm
+    color: '#333',
   },
   productPrice: {
-    color: "#ff6347",
-    fontSize: 14,
+    fontSize: 15,
+    color: '#000000',
+    fontWeight: 'bold',
     marginTop: 5,
+    textAlign: 'center',  // Căn giữa giá sản phẩm
   },
+  productDescription: {
+    fontSize: 14,
+    color: '#666',
+    marginTop: 10,
+    textAlign: 'center',
+  },
+
+  //BottomSheet
   bottomSheetContainer: {
     flex: 1,
     padding: 20,
@@ -342,11 +643,15 @@ const styles = StyleSheet.create({
     fontSize: 16,
   },
   clearCategoryButton: {
-    backgroundColor: "#f44336", // Màu đỏ để nổi bật
+    backgroundColor: "#dc143c", // Màu đỏ để nổi bật
     borderRadius: 8,
-    paddingVertical: 10,
-    marginBottom: 10,
+    paddingVertical: 12,
+    marginTop: 20,
     alignItems: "center",
+    shadowColor: '#000', // Tạo hiệu ứng đổ bóng
+    shadowOffset: { width: 1, height: 2 },
+    shadowOpacity: 0.6,
+    shadowRadius: 4,
   },
   clearCategoryButtonText: {
     color: "#fff",
